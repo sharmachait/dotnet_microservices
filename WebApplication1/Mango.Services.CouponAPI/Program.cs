922b455530cb1835c6642e0a6d626891ebdb7499
beelzekamibub <chait8126@gmail.com>
2023-06-08 15:03:23 +0530

    how to apply migrations automatically when running the app

--- WebApplication1/Mango.Services.CouponAPI/Program.cs
+++ WebApplication1/Mango.Services.CouponAPI/Program.cs
@@ -1,7 +1,16 @@
+using Mango.Services.CouponAPI.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
+builder.Services.AddDbContext<AppDbContext>(
+    option =>
+            {
+                option.UseSqlServer(builder.Configuration.GetConnectionString("Default"));
+            }
+    );
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
@@ -22,4 +31,17 @@ app.UseAuthorization();
 
 app.MapControllers();
 
+ApplyMigration();
+
 app.Run();
+
+
+void ApplyMigration() 
+{
+    using (var scope = app.Services.CreateScope()) {
+        var _db = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+        if (_db.Database.GetPendingMigrations().Count() > 0) { 
+            _db.Database.Migrate();
+        }
+    }
+}
