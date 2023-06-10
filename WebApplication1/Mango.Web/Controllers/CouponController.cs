c975e46c09b84b3ab360a6464aa3a2cdabafc1fa
beelzekamibub <chait8126@gmail.com>
2023-06-10 19:52:57 +0530

    integrated the update endpoint with the frontend

--- WebApplication1/Mango.Web/Controllers/CouponController.cs
+++ WebApplication1/Mango.Web/Controllers/CouponController.cs
@@ -2,6 +2,7 @@
 using Mango.Web.Service.IService;
 using Microsoft.AspNetCore.Mvc;
 using Newtonsoft.Json;
+using System.Collections.Generic;
 
 namespace Mango.Web.Controllers
 {
@@ -18,11 +19,55 @@ namespace Mango.Web.Controllers
             List<CouponDTO>? list = new();
             ResponseDTO? response = await _couponService.GetAllCouponsAsync();
 
-            if (response != null && response.IsSuccess) 
+            if (response != null && response.IsSuccess)
             {
                 list = JsonConvert.DeserializeObject<List<CouponDTO>>(Convert.ToString(response.Result));
             }
             return View(list);
+        }
+
+        public async Task<IActionResult> CouponCreate()
+        {
+
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CouponCreate(CouponDTO model)
+        {
+            if (ModelState.IsValid) {
+                ResponseDTO? response = await _couponService.CreateCouponsAsync(model);
+
+                if (response != null && response.IsSuccess)
+                {
+                    return RedirectToAction(nameof(CouponIndex));
+                }
+            }
+            return View(model);
+        }
+
+        public async Task<IActionResult> CouponDelete(int couponId)
+        {
+            ResponseDTO? response = await _couponService.GetCouponByIdAsync(couponId);
+
+            if (response != null && response.IsSuccess)
+            {
+                CouponDTO? model = JsonConvert.DeserializeObject<CouponDTO>(Convert.ToString(response.Result));
+                return View(model);
+            }
+            return NotFound();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CouponDelete(CouponDTO model)
+        {
+            ResponseDTO? response = await _couponService.DeleteCouponsAsync(model.CouponId);
+
+            if (response != null && response.IsSuccess)
+            {
+                return RedirectToAction(nameof(CouponIndex));
+            }
+            return View(model);
         }
     }
 }
