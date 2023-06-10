2b1592d313511d5b781970971155a0f6d9f7356b
beelzekamibub <chait8126@gmail.com>
2023-06-10 10:47:37 +0530

    implemented the frontend fetching from the api endpoints

---
+++ WebApplication1/Mango.Web/Service/CouponService.cs
@@ -0,0 +1,70 @@
+﻿using Mango.Web.Models;
+using Mango.Web.Service.IService;
+using static Mango.Web.Utility.StaticDetails;
+
+namespace Mango.Web.Service
+{
+    public class CouponService : ICouponService
+    {
+        private readonly IBaseService _baseService;
+        public CouponService(IBaseService baseService)
+        {
+            _baseService = baseService;
+        }
+
+        public async Task<ResponseDTO?> CreateCouponsAsync(CouponDTO couponDTO)
+        {
+            return await _baseService.SendAsync(new RequestDTO()
+            {
+                ApiType = ApiType.POST,
+                Data= couponDTO,
+                Url = CouponAPIBase + "/api/coupon/"
+            });
+        }
+
+        public async Task<ResponseDTO?> DeleteCouponsAsync(int id)
+        {
+            return await _baseService.SendAsync(new RequestDTO()
+            {
+                ApiType = ApiType.DELETE,
+                Url = CouponAPIBase + "/api/coupon/" + id
+            });
+        }
+
+        public async Task<ResponseDTO?> GetAllCouponsAsync()
+        {
+            return await _baseService.SendAsync(new RequestDTO()
+            {
+                ApiType = ApiType.GET,
+                Url=CouponAPIBase+"/api/coupon"
+            });
+        }
+
+        public async Task<ResponseDTO?> GetCouponAsync(string couponCode)
+        {
+            return await _baseService.SendAsync(new RequestDTO() { 
+                ApiType= ApiType.GET,
+                Url=CouponAPIBase+"/api/coupon/GetByCode/"+couponCode
+            });
+        }
+
+        public async Task<ResponseDTO?> GetCouponByIdAsync(int id)
+        {
+            return await _baseService.SendAsync(new RequestDTO()
+            {
+                ApiType = ApiType.GET,
+                Url = CouponAPIBase + "/api/coupon/" + id
+            });
+        }
+
+        public async Task<ResponseDTO?> UpdateCouponsAsync(CouponDTO couponDTO)
+        {
+            return await _baseService.SendAsync(new RequestDTO()
+            {
+                ApiType = ApiType.PUT,
+                Data = couponDTO,
+                Url = CouponAPIBase + "/api/coupon/"
+            });
+        }
+    }
+}
---
+++ WebApplication1/Mango.Web/Service/IService/ICouponService.cs
@@ -0,0 +1,16 @@
+﻿using Mango.Web.Models;
+
+namespace Mango.Web.Service.IService
+{
+    public interface ICouponService
+    {
+        Task<ResponseDTO?> GetCouponAsync(string couponCode);
+        Task<ResponseDTO?> GetAllCouponsAsync();
+        Task<ResponseDTO?> GetCouponByIdAsync(int id);
+        Task<ResponseDTO?> CreateCouponsAsync(CouponDTO couponDTO);
+        Task<ResponseDTO?> UpdateCouponsAsync(CouponDTO couponDTO);
+        Task<ResponseDTO?> DeleteCouponsAsync(int id);
+
+
+    }
+}
