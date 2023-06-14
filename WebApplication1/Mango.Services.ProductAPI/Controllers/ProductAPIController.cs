cb6e786c6bf2f29e1c3a2c4b21744192113b6697
beelzekamibub <chait8126@gmail.com>
2023-06-14 12:16:18 +0530

    get all endpoint for product implemented, implemented automapper for product, aut migration script in program.cs

---
+++ WebApplication1/Mango.Services.ProductAPI/Controllers/ProductAPIController.cs
@@ -0,0 +1,39 @@
+﻿using AutoMapper;
+using Mango.Services.ProductAPI.Data;
+using Mango.Services.ProductAPI.Models;
+using Mango.Services.ProductAPI.Models.DTO;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Mango.Services.ProductAPI.Controllers
+{
+    [Route("api/ProductAPI")]
+    [ApiController]
+    public class ProductAPIController : ControllerBase
+    {
+        private readonly AppDbContext _db;
+        private ResponseDTO _response;
+        private IMapper _mapper;
+        public ProductAPIController(AppDbContext db, IMapper mapper)
+        {
+            _db = db;
+            _response = new ResponseDTO();
+            _mapper = mapper;
+        }
+        [HttpGet]
+        public ResponseDTO Get()
+        {
+            try
+            {
+                IEnumerable<Product> objList = _db.Products.ToList();
+                _response.Result = _mapper.Map<IEnumerable<ProductDTO>>(objList);
+            }
+            catch (Exception ex)
+            {
+                _response.IsSuccess = false;
+                _response.Message = ex.Message;
+            }
+            return _response;
+        }
+    }
+}
