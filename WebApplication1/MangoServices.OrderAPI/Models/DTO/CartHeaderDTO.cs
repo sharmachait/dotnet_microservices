a131e22ef1b562cca568e0ab9db42df8e67334c5
beelzekamibub <chait8126@gmail.com>
2023-07-08 14:40:21 +0530

    checkout

---
+++ WebApplication1/MangoServices.OrderAPI/Models/DTO/CartHeaderDTO.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace Mango.Services.OrderAPI.Models.DTO
+{
+    public class CartHeaderDTO
+    {
+        public int CartHeaderId { get; set; }
+        public string? UserId { get; set; }
+        public string? CouponCode { get; set; }
+        public double Discount { get; set; }
+        public double CartTotal { get; set; }
+        [Required]
+        public string? Name { get; set; }
+        [Required]
+        public string?  Phone { get; set; }
+        [Required]
+        public string?  Email { get; set; }
+    }
+}
